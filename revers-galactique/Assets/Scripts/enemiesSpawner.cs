93cb91636def5076e3a5f0bd39d883506bb1df60
sideking <2234847@cmontmorency.qc.ca>
2024-11-27 22:25:58 -0500

    the second coming

---
+++ revers-galactique/Assets/Scripts/enemiesSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemiesSpawner : MonoBehaviour
+{
+
+    public GameObject secondWave;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && gameObject.tag == "AccessCard")
+        {
+            secondWave.SetActive(true);
+        }
+    }
+
+}
