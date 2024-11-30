4170b66d0f6cf7d0b8555bc01154d23748d67586
sideking <2234847@cmontmorency.qc.ca>
2024-11-30 18:20:16 -0500

    buttons work

--- revers-galactique/Assets/Scripts/LevelSwitcher.cs
+++ revers-galactique/Assets/Scripts/LevelSwitcher.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelSwitcher : MonoBehaviour
 {
+    private UnityEngine.SceneManagement.Scene whereIam;
+
+    void Start()
+    {
+        whereIam = SceneManager.GetActiveScene();
+        Debug.Log("Name: " + whereIam.name);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && whereIam.name == "main")
         {
-            SceneManager.LoadScene("alex"); //replace the scene name
+            SceneManager.LoadScene("alex"); 
         } 
         else if(other.tag == "CityDoor")
         {
             //load last level
         }
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("main"); 
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
     }
 }
