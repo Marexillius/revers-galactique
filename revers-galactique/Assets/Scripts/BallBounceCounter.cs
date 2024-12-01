fd51f12812f058e7187a3d0b945cdde0cac14f26
sideking <2234847@cmontmorency.qc.ca>
2024-12-01 02:34:45 -0500

    might have found it

--- revers-galactique/Assets/Scripts/BallBounceCounter.cs
+++ revers-galactique/Assets/Scripts/BallBounceCounter.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class BallBounceCounter : MonoBehaviour
 {
     private int BounceCounter = 0;
-
+    public GameObject racket;
 
     public Vector3 launchDirection = Vector3.zero; // angle that'll get redefined when it hit the racket
     public float launchForce = 10f; // force
 
+    private void Awake()
+    {
+        StartCoroutine(checkRotation());
+    }
+
+    private IEnumerator checkRotation()
+    {
+        Debug.Log("rotation X : " + racket.transform.localRotation.eulerAngles.x + " rotation Y : " + racket.transform.localRotation.eulerAngles.y + " rotation Z : " + racket.transform.localRotation.eulerAngles.z);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(checkRotation());
+        yield break;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
+        
+
         if (gameObject.tag == "ballPlayer")
         {
             foreach (ContactPoint contact in collision.contacts)
@@ -20,19 +35,19 @@ public class BallBounceCounter : MonoBehaviour
                 //Debug.Log(BounceCounter);
             }
 
-            if (collision.gameObject.name == "racketObject")
+            if (collision.gameObject.name == "racketObject" || collision.gameObject.tag == "racket")
             {
-                //gameObject.transform.position = new Vector3(gameObject.transform.transform.position.x, gameObject.transform.transform.position.y + 1, gameObject.transform.transform.position.z);
+                Vector3 test = new Vector3(gameObject.transform.transform.position.x, gameObject.transform.transform.position.y, gameObject.transform.transform.position.z);
                 // Calculate the surface normal at the point of collision
                 ContactPoint contact = collision.contacts[0];
                 Vector3 surfaceNormal = contact.normal;
 
                 // Calculate a direction perpendicular to the surface normal (90 degrees angle)
                 // You can use the cross product to find a vector perpendicular to the surface normal
-                launchDirection = Vector3.Cross(surfaceNormal, Vector3.up);
+                launchDirection = Vector3.Reflect(surfaceNormal, Vector3.up);
 
                 // Ensure that the launch direction is normalized
-                launchDirection.Normalize();
+                //launchDirection.Normalize();
 
                 gameObject.GetComponent<Rigidbody>().AddForce(launchDirection.normalized * launchForce, ForceMode.Impulse);
             }
@@ -41,6 +56,8 @@ public class BallBounceCounter : MonoBehaviour
             {   
                 /*Destroy(gameObject);*/
             }
+
+
         }
         else if(gameObject.tag == "enemyAttack")
         {
